193e0ed0558a37528336e72687ce3c5c1d7a9f3d
Boniface Kabaso <boniface@kabaso.com>
2017-11-06 10:24:17 +0200

    I added a new Line

--- src/main/java/za/ac/cput/App.java
+++ src/main/java/za/ac/cput/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main(String[] args) {
         System.out.println(" Hello");
         ConcreateClass aObject = new ConcreateClass();
+        System.out.println(" Share This ");
         aObject.run();
     }
 }
