903416551a363a8ad74739e56f658650f9956ab3
Boniface Kabaso <boniface@kabaso.com>
2017-11-06 10:22:18 +0200

    Initial commit

---
+++ src/main/java/za/ac/cput/AbstractClass.java
@@ -0,0 +1,8 @@
+package za.ac.cput;
+
+public abstract class AbstractClass {
+    public abstract void walk();
+    public void crawl(){
+        System.out.println(" Hello World");
+    }
+}
---
+++ src/main/java/za/ac/cput/ConcreateClass.java
@@ -0,0 +1,8 @@
+package za.ac.cput;
+
+public class ConcreateClass implements InterfaceClass {
+    public void run(){
+        System.out.println("hello world");
+
+    }
+}
